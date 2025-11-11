ef5a824e15f0900beabb2003690a1dd7f263664d
Aurelian2025 <aureliang@iceworksbathco.ca>
2025-11-11 15:39:39 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>WarmUp G1 (mini)</h1>
+      <p>If you can see this, the base app is working 🎉</p>
+      <p>Next step: we’ll add a single question.</p>
+    </main>
+  );
+}
